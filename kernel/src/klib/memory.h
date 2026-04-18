f8553f0a5c4dab3f85894fc197e91dbebeecff19
TRWither <iamwither5666@outlook.com>
2026-04-18 11:50:31 +0200

    moved kernel/src/memory.* to klib/

---
+++ kernel/src/klib/memory.h
@@ -0,0 +1,11 @@
+#ifndef KLIB_MEMORY_H
+#define KLIB_MEMORY_H
+
+#include <stddef.h>
+
+void *memcpy(void *restrict dest, const void *restrict src, size_t n);
+void *memset(void *s, int c, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+int memcmp(const void *s1, const void *s2, size_t n);
+
+#endif /* KLIB_MEMORY_H */
